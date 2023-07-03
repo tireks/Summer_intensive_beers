65468263c036440e753050a2f598faac9516a3d7
tireks <33616425+tireks@users.noreply.github.com>
2023-07-03 17:27:07 +0400

    initial commit

---
+++ beer_punk_app/app/src/main/java/com/example/beerpunkapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.beerpunkapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beerpunkapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
