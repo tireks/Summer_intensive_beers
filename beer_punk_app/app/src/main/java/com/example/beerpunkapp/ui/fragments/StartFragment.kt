4a484071104d3d2e483fbb9cca6cdead2cda03c0
tireks <33616425+tireks@users.noreply.github.com>
2023-07-05 00:28:21 +0400

    Пробная версия стартового экрана сделана
    Первая версия реализации RecyclerView на стартовом экране, получилось, но "чудом", не очень понимаю как это работает

--- beer_punk_app/app/src/main/java/com/example/beerpunkapp/ui/fragments/StartFragment.kt
+++ beer_punk_app/app/src/main/java/com/example/beerpunkapp/ui/fragments/StartFragment.kt
@@ -1,14 +1,34 @@
 package com.example.beerpunkapp.ui.fragments
 
+import android.app.Application
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.beerpunkapp.R
 import com.example.beerpunkapp.databinding.FragmentStartBinding
+import com.example.beerpunkapp.model.App
+import com.example.beerpunkapp.model.BeerAdapter
+import com.example.beerpunkapp.model.BeerService
+import com.example.beerpunkapp.model.BeerTestModel
 import com.example.beerpunkapp.utilits.showToast
+import com.github.javafaker.Faker
 
 
 class StartFragment : BaseFragment<FragmentStartBinding>(){
+   // private val adapter by lazy { BeerAdapter() }
+
+    /*private val beerService: BeerService
+        get() = (activity?.applicationContext as App).beerService*/
+    val faker = Faker.instance()
+    val beerList = (1..20).map { BeerTestModel(
+        id = it.toInt(),
+        naming = faker.beer().name(),
+        tags = faker.harryPotter().quote(),
+        description = R.string.recycler_view_description_placeholder.toString(),
+        photo = "https://images.punkapi.com/v2/86.png"
+    ) }.toMutableList()
+
     override fun inflateViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -19,6 +39,12 @@ class StartFragment : BaseFragment<FragmentStartBinding>(){
     override fun onResume() {
         super.onResume()
         setHasOptionsMenu(true)
+        binding.startRecyclerView.adapter = BeerAdapter(::handleCl)
+        (binding.startRecyclerView.adapter as? BeerAdapter)?.beers = beerList
+    }
+
+    private fun handleCl(beerTestModel: BeerTestModel) {
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -38,4 +64,5 @@ class StartFragment : BaseFragment<FragmentStartBinding>(){
             else -> super.onOptionsItemSelected(item)
         }
     }
+
 }
