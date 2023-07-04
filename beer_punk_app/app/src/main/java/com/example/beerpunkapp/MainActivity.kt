b23f097e9e5579b07a4953b8d541cdc5b9272cd0
tireks <33616425+tireks@users.noreply.github.com>
2023-07-04 13:35:35 +0400

    toolbar design
    + placeholders for functionality

--- beer_punk_app/app/src/main/java/com/example/beerpunkapp/MainActivity.kt
+++ beer_punk_app/app/src/main/java/com/example/beerpunkapp/MainActivity.kt
@@ -3,11 +3,29 @@ package com.example.beerpunkapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.beerpunkapp.databinding.ActivityMainBinding
+import com.example.beerpunkapp.ui.fragments.StartFragment
+import com.example.beerpunkapp.utilits.replaceFragment
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunctionality()
+    }
+
+    private fun initFunctionality() {
+        replaceFragment(StartFragment(), R.id.mainDataContainer)
+    }
+
+    private fun initFields() {
+        //TODO("Not yet implemented")
+    }
+
 }
--- beer_punk_app/app/src/main/java/com/example/beerpunkapp/ui/fragments/StartFragment.kt
+++ beer_punk_app/app/src/main/java/com/example/beerpunkapp/ui/fragments/StartFragment.kt
@@ -1,12 +1,11 @@
 package com.example.beerpunkapp.ui.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.beerpunkapp.R
 import com.example.beerpunkapp.databinding.FragmentStartBinding
+import com.example.beerpunkapp.utilits.showToast
 
 
 class StartFragment : BaseFragment<FragmentStartBinding>(){
@@ -17,5 +16,26 @@ class StartFragment : BaseFragment<FragmentStartBinding>(){
         return FragmentStartBinding.inflate(inflater, container, false)
     }
 
+    override fun onResume() {
+        super.onResume()
+        setHasOptionsMenu(true)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.start_action_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.start_menu_btn_search -> {
+                showToast("searchPlaceHolder")
+                true
+            }
+            R.id.start_menu_btn_random -> {
+                showToast("randomizePlaceHolder")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
